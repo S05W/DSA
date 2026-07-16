009417e9ce65a8f049a78f0ee46e92a8650f47b9
Simon Weber <marsbach@gmail.com>
2026-07-16 21:30:40 +0200

    Projektstruktur erstellt

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-
-import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
+import "./styles/variables.css";
+import "./styles/globals.css";
+import "./styles/layout.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
